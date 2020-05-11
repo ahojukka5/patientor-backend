c353ed4b25ff2a6d8c1dee3c156530fde53d402f
Jukka Aho <ahojukka5@gmail.com>
2020-05-11 10:41:41 +0700

    Use cors() in app

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
