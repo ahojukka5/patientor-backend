e8b4b18d1bfcd805d71b950eaa8e28d4f4fbf89d
Jukka Aho <ahojukka5@gmail.com>
2020-10-23 16:57:57 +0700

    Connect patientsRouter

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import pingRouter from './controllers/ping';
 import diagnosesRouter from './controllers/diagnoses';
+import patientsRouter from './controllers/patients';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api/ping', pingRouter);
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 export default app;
