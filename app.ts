7acd94e4f45918b9a4f5c23b16f116f487a9c4ca
Jukka Aho <ahojukka5@gmail.com>
2020-10-23 15:42:46 +0700

    Add route /api/diagnoses

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import pingRouter from './controllers/ping';
+import diagnosesRouter from './controllers/diagnoses';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api/ping', pingRouter);
+app.use('/api/diagnoses', diagnosesRouter);
 
 export default app;
