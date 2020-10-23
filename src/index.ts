209c9dcd85533a689f26fdfcddaafd5ff3a75242
Jukka Aho <ahojukka5@gmail.com>
2020-10-23 18:26:07 +0700

    Move config.ts to src-root

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from './app';
-import config from './utils/config';
+import config from './config';
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
