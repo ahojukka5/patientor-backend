d25a6ca81b75426cab061d5523ee39fe39b830d8
Jukka Aho <ahojukka5@gmail.com>
2020-10-23 17:39:16 +0700

    Add POST method for patients controller

--- src/controllers/patients.ts
+++ src/controllers/patients.ts
@@ -8,4 +8,16 @@ router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitiveEntries());
 });
 
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatientEntry = patientService.addEntry({
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  });
+  res.send(newPatientEntry);
+});
+
 export default router;
