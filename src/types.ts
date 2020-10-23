0aabb93b2703b0f9e10fc877fcf2c5a51f9155bf
Jukka Aho <ahojukka5@gmail.com>
2020-10-23 16:56:06 +0700

    Add new type interface PatientEntry

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,13 @@ export interface DiagnoseEntry {
   name: string;
   latin?: string;
 }
+
+export interface PatientEntry {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
+
