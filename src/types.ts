e2f20507aef16a54aecd623c171ca056521f273c
Jukka Aho <ahojukka5@gmail.com>
2020-10-25 19:49:35 +0700

    Add entries to Patient interface

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,10 @@ export interface Diagnose {
   latin?: string;
 }
 
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {}
+
 export interface Patient {
   id: string;
   name: string;
@@ -11,9 +15,10 @@ export interface Patient {
   ssn: string;
   gender: string;
   occupation: string;
+  entries: Entry[];
 }
 
-export type NonSensitivePatient = Omit<Patient, 'ssn'>;
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
 
--- src/utils.ts
+++ src/utils.ts
@@ -56,6 +56,7 @@ const toNewPatient = (object: any): NewPatient => {
     ssn: parseSSN(object.ssn),
     gender: parseGender(object.gender),
     occupation: parseOccupation(object.occupation),
+    entries: []
   };
 };
 
