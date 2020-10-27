5717b12e28a37ff868ed3da220715379899c4e0b
Jukka Aho <ahojukka5@gmail.com>
2020-10-28 00:19:37 +0700

    Update test for entry point /ping

--- tests/ping.test.ts
+++ tests/ping.test.ts
@@ -1,14 +1,11 @@
-import express from 'express';
-import pingRouter from '../src/controllers/ping';
+import app from '../src/app';
 import supertest from 'supertest';
 
-const app = express();
-app.use(pingRouter);
 const request = supertest(app);
 
 describe('ping endpoint', () => {
-  test('should return pong form ping', async () => {
-    const response = await request.get('/');
+  test('should return pong from ping', async () => {
+    const response = await request.get('/api/ping');
     const expected = 'pong';
     expect(response.text).toEqual(expected);
   });
