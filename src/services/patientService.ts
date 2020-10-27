b7b327840783cca1d58f7cf474be21148cd4006b
Jukka Aho <ahojukka5@gmail.com>
2020-10-28 02:42:33 +0700

    Add service to add new patient entry

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -2,7 +2,7 @@ import { generateUUIDv4 } from '@bitjourney/uuid-v4';
 
 import patientData from '../../data/patients';
 
-import { NonSensitivePatient, Patient, NewPatient } from '../types';
+import { NonSensitivePatient, Patient, NewPatient, Entry, NewEntry } from '../types';
 
 import { toNewPatient } from '../utils';
 
@@ -37,9 +37,21 @@ const findById = (id: string): Patient | undefined => {
   return patient;
 };
 
+const addPatientEntry = (patientId: string, entry: NewEntry): Entry => {
+  const newPatientEntry = { id: generateUUIDv4(), ...entry } as Entry;
+  const patient = findById(patientId);
+  if (!patient) {
+    throw Error(`Patient with id ${patientId} not found.`);
+  }
+  const entries = patient.entries;
+  entries.push(newPatientEntry);
+  return newPatientEntry;
+};
+
 export default {
   getEntries,
   getNonSensitiveEntries,
   addPatient,
+  addPatientEntry,
   findById,
 };
