1e94cbfcb896f4cfc8f302c1cde2076e1b2bc144
Jukka Aho <ahojukka5@gmail.com>
2020-10-28 00:40:25 +0700

    Add test for /patients POST

--- tests/patients.test.ts
+++ tests/patients.test.ts
@@ -8,4 +8,15 @@ describe('patients endpoint', () => {
     const response = await request.get('/api/patients');
     expect(response.body[0].name).toEqual('John McClane');
   });
+  test('should add new patient', async () => {
+    const payload = {
+      name: 'John Doe',
+      dateOfBirth: '2000-01-01',
+      ssn: '1234',
+      gender: 'male',
+      occupation: 'engineer',
+    };
+    const response = await request.post('/api/patients').send(payload);
+    expect(response.body.name).toEqual('John Doe');
+  });
 });
